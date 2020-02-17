365ab6a02280caad6aab205f42ad22f598dc2263
Luca <luca-marconi@hotmail.com>
2020-02-17 16:17:35 +0100

    completamento esercizio

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var primoDato = prompt('Come ti chiami?');
+var secondoDato = prompt('Il tuo cognome?');
+var terzoDato = prompt('Qual\'è il tuo colore preferito?');
+
+document.getElementById('password').innerHTML = primoDato + secondoDato + terzoDato + '20';
