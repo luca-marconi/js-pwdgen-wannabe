a38c98ce1d9d2de69560f5ba19511a486b5548c9
Luca <luca-marconi@hotmail.com>
2020-02-17 16:49:33 +0100

    completamento esercizi secondari

--- js/main.js
+++ js/main.js
@@ -3,3 +3,28 @@ var secondoDato = prompt('Il tuo cognome?');
 var terzoDato = prompt('Qual\'è il tuo colore preferito?');
 
 document.getElementById('password').innerHTML = primoDato + secondoDato + terzoDato + '20';
+
+// var primoDato = prompt('Come ti chiami?');
+// var secondoDato = prompt('Il tuo cognome?');
+// var terzoDato = prompt('Qual\'è il tuo colore preferito?');
+// var data = new Date();
+// var annoCorrente = data.getFullYear();
+//
+// document.getElementById('password').innerHTML = primoDato + secondoDato + terzoDato + annoCorrente;
+
+// var primoDato = prompt('Come ti chiami?');
+// var secondoDato = prompt('Il tuo cognome?');
+// var terzoDato = prompt('Qual\'è il tuo colore preferito?');
+// var data = new Date();
+// var annoCorrente = data.getFullYear();
+//
+// document.getElementById('password').innerHTML = primoDato + ';' + secondoDato + ';' +  terzoDato + ';' +  annoCorrente;
+
+// var primoDato = prompt('Come ti chiami?');
+// var secondoDato = prompt('Il tuo cognome?');
+// var terzoDato = prompt('Qual\'è il tuo colore preferito?');
+// var data = new Date();
+// var annoCorrente = data.getFullYear();
+// var sottr = annoCorrente - 2000;
+//
+// document.getElementById('password').innerHTML = primoDato + ';' + secondoDato + ';' +  terzoDato + ';' +  sottr;
